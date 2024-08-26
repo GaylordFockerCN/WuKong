c754748c75951e33b4be8b42ed75d5bb63355414
P1nero <1070402601@qq.com>
2024-08-27 03:25:45 +0800

    添加光的残影，准备生成音乐

---
+++ src/main/java/com/p1nero/wukong/data/DataGenerators.java
@@ -0,0 +1,22 @@
+//package com.p1nero.wukong.data;
+//
+//import com.p1nero.wukong.WukongMoveset;
+//import com.p1nero.wukong.data.sound.SoundGenerator;
+//import net.minecraft.data.DataGenerator;
+//import net.minecraftforge.common.data.ExistingFileHelper;
+//import net.minecraftforge.eventbus.api.SubscribeEvent;
+//import net.minecraftforge.fml.common.Mod;
+//import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+//
+//
+//@Mod.EventBusSubscriber(modid = WukongMoveset.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+//public class DataGenerators {
+//    @SubscribeEvent
+//    public static void gatherData(GatherDataEvent event) {
+//        DataGenerator generator = event.getGenerator();
+////        PackOutput output = generator.getPackOutput();
+////        ExistingFileHelper helper = event.getExistingFileHelper();
+////        generator.addProvider(event.includeClient(), new SoundGenerator(output, helper));
+//    }
+//
+//}
---
+++ src/main/java/com/p1nero/wukong/data/sound/SoundGenerator.java
@@ -0,0 +1,24 @@
+//package com.p1nero.wukong.data.sound;
+//
+//import net.minecraftforge.common.data.ExistingFileHelper;
+//
+//public class SoundGenerator extends SoundProvider {
+//
+//    public SoundGenerator(PackOutput output, ExistingFileHelper helper) {
+//        super(output, helper);
+//    }
+//
+//    @Override
+//    public void registerSounds() {
+//        this.generateNewSoundWithSubtitle(LMMSounds.GROUND_SLAM, "sfx/ground_slam", 1);
+//        this.generateNewSoundWithSubtitle(LMMSounds.GROUND_SLAM_SMALL, "sfx/ground_slam_small", 1);
+//        this.generateNewSoundWithSubtitle(LMMSounds.BREAK, "break", 1);
+//        this.generateNewSoundWithSubtitle(LMMSounds.LIGHT, "light", 1);
+//        this.generateNewSoundWithSubtitle(LMMSounds.SKILL, "skill", 1);
+//        this.generateNewSoundWithSubtitle(LMMSounds.ATTACK, "attack", 4);
+//        this.generateNewSoundWithSubtitle(LMMSounds.LINE1, "1", 1);
+//        this.generateNewSoundWithSubtitle(LMMSounds.LINE2, "2", 1);
+//        this.generateNewSoundWithSubtitle(LMMSounds.LINE3, "3", 1);
+//        this.generateNewSoundWithSubtitle(LMMSounds.BGM, "bgm", 1);
+//    }
+//}
