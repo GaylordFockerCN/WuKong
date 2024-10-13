ed5272502d6d6dbe00a170c0d4bf9ffe3897d36c
P1nero <1070402601@qq.com>
2024-10-13 22:01:55 +0800

    亢金棍 和 赤潮 模型

---
+++ src/main/java/com/p1nero/wukong/item/RedTide.java
@@ -0,0 +1,10 @@
+package com.p1nero.wukong.item;
+
+import net.minecraft.world.item.Tier;
+import yesman.epicfight.world.item.WeaponItem;
+
+public class RedTide extends WeaponItem {
+    public RedTide(Tier tier, int damageIn, float speedIn, Properties builder) {
+        super(tier, damageIn, speedIn, builder);
+    }
+}
